db4f3bf16b741565c5a4c5d52c64a434d1fd5b41
21CHANCHAL21 <95063469+21CHANCHAL21@users.noreply.github.com>
2023-11-04 11:49:49 +0530

    Add files via upload

---
+++ Task6/src/ResumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+   
+   "name":"Chanchal",
+   "role":"Student of Cyber Security and Software Engineering",
+   "roleDescription":"I like to learn and create Software Application While maintaining Security Postures. In my free time I do Web Penetration Testing and Learning New Skills.",
+   "socialLinks":[
+    {
+   "name":"linkedin",
+   "url":"https://www.linkedin.com/in/chanchal-sharma-6a2625224/",
+   "className":"fa fa-linkedin"
+    },
+    {
+   "name":"github",
+   "url":"https://github.com/21CHANCHAL21",
+   "className":"fa fa-github"
+    }
+    ],
+   "aboutme":"I am a pre-final year student and currently pursuing M.Tech(Integrated) Computer Science & Engineering Specialization in Cyber Security from Vellore Institute of Technology-Bhopal. ",
+   "address":"India",
+   "website":"https://github.com/21CHANCHAL21",
+   "education":[
+    {
+   "UniversityName":"Vellore Institute of Technology - Bhopal",
+   "specialization":"Cyber Securiy",
+   "MonthOfPassing":"July",
+   "YearOfPassing":"2020-Present",
+   "Achievements":"100% Scholarship STARS Student || 8.50 CGPA"
+    },
+    {
+   "UniversityName":"Govt. High Secondary School Baraundha (M.P.)",
+   "specialization":"Intermediate - PCM",
+   "MonthOfPassing":"July",
+   "YearOfPassing":"2020",
+   "Achievements":"92.4 % in 12th Class - Distt. Topper"
+    }
+    ],
+   "work":[
+    {
+   "CompanyName":"Bharat Intern",
+   "specialization":"Internship - Full Stack Web Development",
+   "MonthOfLeaving":"Sept-October",
+   "YearOfLeaving":"2023",
+   // "Achievements":"Cloud Computing | Practical Ethical Hacking | Information Security "
+    },
+    {
+   "CompanyName":"CodSoft",
+   "specialization":"Internship - Web Development",
+   "MonthOfLeaving":"Sept-Nov",
+   "YearOfLeaving":"2023",
+   "Achievements":"ReactJs, NodeJs | Practical Experience | Web development Skills"
+    }
+    ],
+   "skillsDescription":"Skills",
+   "skills":[
+    {
+   "skillname":"Java"
+    },
+    {
+   "skillname":"Python"
+    }
+    ,
+    {
+   "skillname":"Machine_Learning"
+    },
+    {
+   "skillname":"MERN_Full_Stack"
+    }
+    ,
+    
+    {
+   "skillname":"Web_Penetration_Testing"
+    }
+    ],
+   "portfolio":[
+    {
+   "name":"Project1",
+   "description":"Port Scanner- Created a GUI-based application using Python which will helpful in scanning the ports and  tell to the user what are ports are open and closed.",
+   "imgurl":"images/portfolio/ProjectOne.jpg"
+    },
+    {
+   "name":"Project2",
+   "description":"Antivirus- To detect malware in the Windows operating system.",
+   "imgurl":"images/portfolio/ProjectTwo.jpg"
+    }
+    ],
+   "testimonials":[
+    {
+   "description":"This is Portfolio of Chanchak Sharma",
+   "name":"Software Engineering || Cyber Security || Machine Learning"
+    },
+    {
+   "description":"Like to Read Books, and playing chess",
+   "name":"Innovation || Work Hard"
+    }
+    ]
+    }
+    export default resumeData
